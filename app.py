8b83f37f5e66625e7a6f62212482420e649018fc
LokeshDandumahanti <146311601+LokeshDandumahanti@users.noreply.github.com>
2024-05-20 10:40:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 import joblib
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
-import scikit-learn as sklearn
 # Load the model and columns
 X_columns = pd.read_csv("dora.csv")
 df12 = pd.read_csv("output.csv")
